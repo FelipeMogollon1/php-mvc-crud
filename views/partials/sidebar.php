e97d68777c06869da77deac6c1ad06a77e739007
Felipe Mogollón <94751785+FelipeMogollon1@users.noreply.github.com>
2024-11-13 08:44:22 -0500

    feat: update sidebar

--- views/partials/sidebar.php
+++ views/partials/sidebar.php
@@ -1,3 +1,6 @@
+<?php
+require_once __DIR__ . '/../../functions/UrlHelper.php';
+?>
 <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="height: 100vh; width: 280px;">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
         <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
@@ -6,19 +9,19 @@
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item">
-            <a href="/Proyect_PHP_CRUD/public/users/" class="nav-link active" aria-current="page">
+            <a href="<?php echo base_url(); ?>/users/" class="nav-link active" aria-current="page">
                 <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
                 Users
             </a>
         </li>
         <li>
-            <a href="/Proyect_PHP_CRUD/public/cities/" class="nav-link text-white">
+            <a href="<?php echo base_url(); ?>/cities/" class="nav-link text-white">
                 <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
                 Cities
             </a>
         </li>
         <li>
-            <a href="/Proyect_PHP_CRUD/public/jobs/" class="nav-link text-white">
+            <a href="<?php echo base_url(); ?>/jobs/" class="nav-link text-white">
                 <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
                 Jobs
             </a>
